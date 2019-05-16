934580f2bff8634b71cccee8b3615e3660e0d1ca
Thomas Moulard <tmoulard@amazon.com>
2019-05-16 14:31:13 -0700

    Add copyright header to main.cpp
    Signed-off-by: Thomas Moulard <tmoulard@amazon.com>

--- src/runner/runner.hpp
+++ src/runner/runner.hpp
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#ifdef ROS_SEC_TEST__RUNNER_RUNNER_HPP_
+#ifndef ROS_SEC_TEST__RUNNER_RUNNER_HPP_
 #define ROS_SEC_TEST__RUNNER_RUNNER_HPP_
 #include <memory>
 #include <string>
