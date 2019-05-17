c33310aee7e2b62fa2b742ed0e7376b3b3ae1b6e
Thomas Moulard <tmoulard@amazon.com>
2019-05-16 19:18:22 -0700

    Simplify main code
    Signed-off-by: Thomas Moulard <tmoulard@amazon.com>

--- src/runner/main.cpp
+++ src/runner/main.cpp
@@ -11,41 +11,20 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include <cstdio>
 #include <cstdlib>
-#include <memory>
-#include <string>
-#include <thread>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
 
-#include "rclcpp/rclcpp.hpp"
-#include "rcutils/logging_macros.h"
-
+#include "rclcpp/utilities.hpp"
 #include "runner/runner.hpp"
 
 using ros_sec_test::runner::Runner;
-using ros_sec_test::utilities::LifecycleServiceClient;
-
-void run_script(Runner & runner);
 
 int main(int argc, char * argv[])
 {
-  // Force flush of the stdout buffer.
-  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);  // Force flush of the stdout buffer.
   rclcpp::init(argc, argv);
-  // Get set of attack to start
-  std::unordered_set<std::string> node_names;
-  for (int i = 1; i < argc; i++) {
-    node_names.insert(std::string(argv[i]));
-  }
-
-  std::vector<std::string> initialized_nodes;
-  for (const auto & node_name : node_names) {
-    initialized_nodes.push_back(node_name);
-  }
-  Runner runner(initialized_nodes);
+  // FIXME: the list of attacks should be made configurable using ROS 2 params.
+  Runner runner({});
   runner.spin();
   rclcpp::shutdown();
   return EXIT_SUCCESS;
