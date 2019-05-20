5e5a00a73453571823f4ab845406cadf7a1464a1
Thomas Moulard <tmoulard@amazon.com>
2019-05-20 14:08:02 -0700

    Add test_client_utils.cpp
    Signed-off-by: Thomas Moulard <tmoulard@amazon.com>

--- test/utilities/test_client_utils.cpp
+++ test/utilities/test_client_utils.cpp
@@ -11,11 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include <gtest/gtest.h>
+
+#include <atomic>
 #include <chrono>
 #include <memory>
 #include <string>
-
-#include <gtest/gtest.h>
 
 #include "rcl_interfaces/srv/list_parameters.hpp"
 #include "rclcpp/executors/single_threaded_executor.hpp"
@@ -27,43 +28,93 @@
 using rcl_interfaces::srv::ListParameters;
 using rclcpp::executors::SingleThreadedExecutor;
 using ros_sec_test::utilities::invoke_service_once_ready;
+using ros_sec_test::utilities::wait_for_result;
 
-TEST(invoke_service_once_ready, check_wait_logic) {
+TEST(invoke_service_once_ready, service_not_available) {
+  using namespace std::chrono_literals;
+  rclcpp::init(0, nullptr);
+  {
+    auto node = rclcpp::Node::make_shared("test_node");
+    auto client = node->create_client<ListParameters>("test_service");
+    auto request = std::make_shared<ListParameters::Request>();
+    auto response = invoke_service_once_ready(node.get(), client.get(), request, 100ms);
+    RCLCPP_INFO(node->get_logger(), "The following reported error is expected.");
+    EXPECT_EQ(response.get(), nullptr);
+  }
+  rclcpp::shutdown();
+}
+
+TEST(invoke_service_once_ready, invoke_service_successfully) {
   using namespace std::chrono_literals;
   rclcpp::init(0, nullptr);
   {
     SingleThreadedExecutor executor;
     auto node = rclcpp::Node::make_shared("test_node");
     executor.add_node(node);
-    auto callback =
-      [](const ListParameters::Request::SharedPtr, ListParameters::Response::SharedPtr) {
-      };
-    auto srv = node->create_service<ListParameters>("test_service", callback);
 
     std::promise<void> thread_promise;
     std::shared_future<void> future = thread_promise.get_future();
+    std::atomic_bool service_has_been_invoked(false);
+
+    auto callback =
+      [&service_has_been_invoked](const ListParameters::Request::SharedPtr,
+        ListParameters::Response::SharedPtr) {
+        service_has_been_invoked.store(true);
+      };
+    auto srv = node->create_service<ListParameters>("test_service", callback);
 
     std::thread thread_spin([&executor, &future, &node]() {
         RCLCPP_INFO(node->get_logger(), "Spin thread started.");
         executor.spin_until_future_complete(future, 10ms);
         RCLCPP_INFO(node->get_logger(), "Spin thread ended.");
       });
-    std::thread thread_client([&node]() {
-        RCLCPP_INFO(node->get_logger(), "Client thread started.");
-        auto client = node->create_client<ListParameters>("test_service");
-        auto request = std::make_shared<ListParameters::Request>();
-        invoke_service_once_ready(node.get(), client.get(), request, 10s);
-        RCLCPP_INFO(node->get_logger(), "Client thread ended.");
-      });
 
-    thread_client.join();
+    auto client = node->create_client<ListParameters>("test_service");
+    auto request = std::make_shared<ListParameters::Request>();
+    auto response = invoke_service_once_ready(node.get(), client.get(), request, 10s);
+    EXPECT_NE(response.get(), nullptr);
+    EXPECT_TRUE(service_has_been_invoked.load());
+
     RCLCPP_INFO(node->get_logger(), "Thread promise set.");
     thread_promise.set_value();
     thread_spin.join();
-
   }
   rclcpp::shutdown();
 }
+
+TEST(wait_for_result, check_timeout) {
+  using namespace std::chrono_literals;
+  rclcpp::init(0, nullptr);
+  {
+    std::promise<void> promise;
+    std::future<void> future = promise.get_future();
+    EXPECT_EQ(wait_for_result(future, 1ms), std::future_status::timeout);
+  }
+  rclcpp::shutdown();
+}
+
+TEST(wait_for_result, check_ready) {
+  using namespace std::chrono_literals;
+  rclcpp::init(0, nullptr);
+  {
+    std::promise<void> promise;
+    std::future<void> future = promise.get_future();
+    promise.set_value();
+    EXPECT_EQ(wait_for_result(future, 1ms), std::future_status::ready);
+  }
+  rclcpp::shutdown();
+}
+
+// Unlike std::future::wait_for(), wait_for_result should never block if ROS is not
+// initialized or already shut down.
+TEST(wait_for_result, check_rclcpp_not_ok) {
+  using namespace std::chrono_literals;
+  EXPECT_FALSE(rclcpp::ok());
+  std::promise<void> promise;
+  std::future<void> future = promise.get_future();
+  EXPECT_EQ(wait_for_result(future, 10s), std::future_status::timeout);
+}
+
 
 int main(int argc, char ** argv)
 {
