e87a6d383178503affb1f5d940e2ffa45c9fdcc7
ryanewel <43657459+ryanewel@users.noreply.github.com>
2019-05-23 15:46:00 -0700

    Add CPU resource attack (#7)
    Add CPU resource attack
    
    Signed-off-by: Ryan Newell <ryanewel@amazon.com>

---
+++ include/ros_sec_test/attacks/resources/cpu/component.hpp
@@ -0,0 +1,95 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef ROS_SEC_TEST__ATTACKS__RESOURCES__CPU__COMPONENT_HPP_
+#define ROS_SEC_TEST__ATTACKS__RESOURCES__CPU__COMPONENT_HPP_
+#include <mutex>
+#include <thread>
+#include <vector>
+
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+#include "rcpputils/thread_safety_annotations.hpp"
+
+namespace ros_sec_test
+{
+namespace attacks
+{
+namespace resources
+{
+namespace cpu
+{
+
+/// This attack tries to use all available CPU resources.
+/**
+ * WARNING: this attack will try to use all of your CPU resources!
+ *
+ * The attack will spawn threads doing arbitrary work.
+ *
+ * When the node shutdowns, the node will attempt to kill
+ * the threads it is using.
+ */
+class Component : public rclcpp_lifecycle::LifecycleNode
+{
+public:
+  Component();
+  explicit Component(std::size_t max_num_threads);
+  Component(const Component &) = delete;
+  Component & operator=(const Component &) = delete;
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_configure(const rclcpp_lifecycle::State & /* state */) final;
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_activate(const rclcpp_lifecycle::State & /* state */) final;
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_deactivate(const rclcpp_lifecycle::State & /* state */) final;
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_cleanup(const rclcpp_lifecycle::State & /* state */) final;
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_shutdown(const rclcpp_lifecycle::State & /* state */ state) final;
+
+private:
+  /// Spawn another thread.
+  void run_periodic_attack();
+
+  /// Run an infinite loop of arbitrary work
+  static void consume_cpu_resources();
+
+  /// Join threads, clear vector, reset timer
+  void terminate_attack_and_cleanup_resources();
+
+  /// Maximum number of threads to spawn
+  const std::size_t max_num_threads_;
+
+  /// Manages thread safety for threads_
+  mutable std::mutex mutex_;
+
+  /// Threads used to occupy CPU resources
+  std::vector<std::thread> threads_ RCPPUTILS_TSA_GUARDED_BY(mutex_);
+
+  /// Timer controlling how often we spawn another thread.
+  rclcpp::TimerBase::SharedPtr timer_ RCPPUTILS_TSA_GUARDED_BY(mutex_);
+};
+
+}  // namespace cpu
+}  // namespace resources
+}  // namespace attacks
+}  // namespace ros_sec_test
+
+#endif  // ROS_SEC_TEST__ATTACKS__RESOURCES__CPU__COMPONENT_HPP_
--- src/attacks/resources/cpu/component.cpp
+++ src/attacks/resources/cpu/component.cpp
@@ -11,24 +11,27 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "ros_sec_test/attacks/resources/disk/component.hpp"
+#include "ros_sec_test/attacks/resources/cpu/component.hpp"
 
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
 
 #include <chrono>
 #include <memory>
+#include <mutex>
 #include <string>
 #include <thread>
+#include <utility>
+#include <vector>
 
+#include "lifecycle_msgs/msg/state.hpp"
 #include "lifecycle_msgs/msg/transition.hpp"
 #include "rclcpp/publisher.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "rclcpp_lifecycle/lifecycle_publisher.hpp"
 #include "rcutils/logging_macros.h"
+#include "rcpputils/thread_safety_annotations.hpp"
 
 using namespace std::chrono_literals;
 
@@ -40,20 +43,33 @@ namespace attacks
 {
 namespace resources
 {
-namespace disk
+namespace cpu
 {
-
+// By default start with twice the maximum supported number of threads. Hardware concurrency
+// isn't guaranteed to be accurate so the maximum number of threads is doubled to avoid
+// the attack failing.
 Component::Component()
+: Component(2 * std::thread::hardware_concurrency()) {}
+
+Component::Component(std::size_t max_num_threads)
 : rclcpp_lifecycle::LifecycleNode(
-    "resources_disk", "", rclcpp::NodeOptions().use_intra_process_comms(
-      true)) {}
+    "resources_cpu", "", rclcpp::NodeOptions().use_intra_process_comms(
+      true)),
+  max_num_threads_(max_num_threads),
+  mutex_(),
+  threads_(),
+  timer_() {}
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 Component::on_configure(const rclcpp_lifecycle::State & /* state */)
+RCPPUTILS_TSA_REQUIRES(!mutex_)
 {
   RCLCPP_INFO(get_logger(), "on_configure() is called.");
-  timer_ = this->create_wall_timer(
-    1s, [this] {run_periodic_attack();});
+  auto callback = [this] {run_periodic_attack();};
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    timer_ = this->create_wall_timer(1s, std::move(callback));
+  }
   return CallbackReturn::SUCCESS;
 }
 
@@ -61,7 +77,6 @@ rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 Component::on_activate(const rclcpp_lifecycle::State & /* state */)
 {
   RCLCPP_INFO(get_logger(), "on_activate() is called.");
-  fd_ = open("attack.dat", O_WRONLY | O_CREAT);
   return CallbackReturn::SUCCESS;
 }
 
@@ -76,32 +91,62 @@ rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 Component::on_cleanup(const rclcpp_lifecycle::State & /* state */)
 {
   RCLCPP_INFO(get_logger(), "on_cleanup() is called.");
+  terminate_attack_and_cleanup_resources();
   return CallbackReturn::SUCCESS;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 Component::on_shutdown(const rclcpp_lifecycle::State & /* state */)
 {
-  timer_.reset();
   RCLCPP_INFO(get_logger(), "on_shutdown() is called.");
+  terminate_attack_and_cleanup_resources();
   return CallbackReturn::SUCCESS;
 }
 
-void
-Component::run_periodic_attack() const
+void Component::terminate_attack_and_cleanup_resources() RCPPUTILS_TSA_REQUIRES(!mutex_)
 {
-  // Get file size
-  struct stat stat_buf;
-  int rc = fstat(fd_, &stat_buf);
-  RCLCPP_INFO(get_logger(), "Current disk size %d", stat_buf.st_size);
-  if (rc == 0) {
-    // Allocate an additonal 100MB
-    posix_fallocate(fd_, stat_buf.st_size, 100 * 1024 * 1024);
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    timer_.reset();
   }
-  // we should stop the attack if the call fails.
+  std::vector<std::thread> threads;
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    threads = std::move(threads_);
+  }
+  for (auto & thread : threads) {
+    thread.join();
+  }
+  threads.clear();
 }
 
-}  // namespace disk
+void Component::run_periodic_attack() RCPPUTILS_TSA_REQUIRES(!mutex_)
+{
+  auto thread = std::thread([this] {consume_cpu_resources();});
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (max_num_threads_ > threads_.size()) {
+      threads_.push_back(std::move(thread));
+    }
+  }
+}
+
+
+void Component::consume_cpu_resources()
+{
+  int i_sum = 0;
+  for (int i = 0;; i++) {
+    if (!rclcpp::ok()) {
+      return;
+    }
+    // It's fine if this overflows
+    i_sum += i;
+    // Prevent optimization
+    asm ("nop");
+  }
+}
+
+}  // namespace cpu
 }  // namespace resources
 }  // namespace attacks
 }  // namespace ros_sec_test
@@ -111,5 +156,5 @@ Component::run_periodic_attack() const
 // Register the component with class_loader.
 // This acts as a sort of entry point, allowing the component to be discoverable when its library
 // is being loaded into a running process.
-CLASS_LOADER_REGISTER_CLASS(ros_sec_test::attacks::resources::disk::Component,
+CLASS_LOADER_REGISTER_CLASS(ros_sec_test::attacks::resources::cpu::Component,
   rclcpp_lifecycle::LifecycleNode)
