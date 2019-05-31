74f512d71499bc1c7d8654a6a69c64554adac14d
Ryan Newell <ryanewel@amazon.com>
2019-05-31 22:16:16 +0000

    Refactor test cases
    Signed-off-by: Ryan Newell <ryanewel@amazon.com>

--- test/ros_sec_test/attacks/resources/disk/test_component.cpp
+++ test/ros_sec_test/attacks/resources/disk/test_component.cpp
@@ -17,78 +17,21 @@
 #include <memory>
 #include <string>
 
-#include "ros_sec_test/attacks/resources/memory/component.hpp"
+#include "ros_sec_test/attacks/resources/disk/component.hpp"
+#include "test_utilities/utility_fixtures.hpp"
 
 #include "lifecycle_msgs/msg/state.hpp"
 #include "lifecycle_msgs/msg/transition.hpp"
 #include "rclcpp/executors/single_threaded_executor.hpp"
 #include "rclcpp/node.hpp"
 
-using MemoryNode = ros_sec_test::attacks::resources::memory::Component;
+using DiskNode = ros_sec_test::attacks::resources::disk::Component;
 using lifecycle_msgs::msg::State;
 using lifecycle_msgs::msg::Transition;
-
-class ROSTestingFixture : public ::testing::Test
-{
-protected:
-  ROSTestingFixture()
-  {
-    rclcpp::init(0, nullptr);
-  }
-
-  ~ROSTestingFixture()
-  {
-    rclcpp::shutdown();
-  }
-};
-
-
-class NodeConfigurationFixture : public ROSTestingFixture
-{
-protected:
-  rclcpp::executors::SingleThreadedExecutor executor_;
-  rclcpp::Node::SharedPtr node_;
-  std::promise<void> thread_promise_;
-  std::shared_future<void> future_;
-
-  void SetUp() override
-  {
-    node_ = rclcpp::Node::make_shared("test_node");
-    executor_.add_node(node_);
-    future_ = thread_promise_.get_future();
-  }
-
-  void add_node_to_executor(rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node)
-  {
-    executor_.add_node(node);
-  }
-
-  void spin_executor_until(std::shared_future<void> & future)
-  {
-    using namespace std::chrono_literals;
-    std::thread thread_spin([this, &future]() {
-        RCLCPP_INFO(this->node_->get_logger(), "Spin thread started.");
-        this->executor_.spin_until_future_complete(future, 100ms);
-        RCLCPP_INFO(this->node_->get_logger(), "Spin thread ended.");
-      });
-    thread_spin.join();
-  }
-  void start_executor()
-  {
-    spin_executor_until(future_);
-  }
-  void stop_executor()
-  {
-    thread_promise_.set_value();
-  }
-  void TearDown() override
-  {
-    stop_executor();
-  }
-};
+using ros_sec_test::test::test_utilities::NodeConfigurationFixture;
 
 TEST_F(NodeConfigurationFixture, check_configuring_transition) {
-  auto attack_node = std::make_shared<MemoryNode>(0);
+  auto attack_node = std::make_shared<DiskNode>();
   add_node_to_executor(attack_node->get_node_base_interface());
   start_executor();
   ASSERT_EQ(State::PRIMARY_STATE_INACTIVE, attack_node->trigger_transition(
@@ -96,7 +39,7 @@ TEST_F(NodeConfigurationFixture, check_configuring_transition) {
 }
 
 TEST_F(NodeConfigurationFixture, check_activating_transition) {
-  auto attack_node = std::make_shared<MemoryNode>(0);
+  auto attack_node = std::make_shared<DiskNode>();
   add_node_to_executor(attack_node->get_node_base_interface());
   start_executor();
   attack_node->trigger_transition(
@@ -107,7 +50,7 @@ TEST_F(NodeConfigurationFixture, check_activating_transition) {
 }
 
 TEST_F(NodeConfigurationFixture, check_deactivating_transition) {
-  auto attack_node = std::make_shared<MemoryNode>(0);
+  auto attack_node = std::make_shared<DiskNode>();
   add_node_to_executor(attack_node->get_node_base_interface());
   start_executor();
   attack_node->trigger_transition(
@@ -121,7 +64,7 @@ TEST_F(NodeConfigurationFixture, check_deactivating_transition) {
 }
 
 TEST_F(NodeConfigurationFixture, check_cleaningup_transition) {
-  auto attack_node = std::make_shared<MemoryNode>(0);
+  auto attack_node = std::make_shared<DiskNode>();
   add_node_to_executor(attack_node->get_node_base_interface());
   start_executor();
   attack_node->trigger_transition(
@@ -132,7 +75,7 @@ TEST_F(NodeConfigurationFixture, check_cleaningup_transition) {
 }
 
 TEST_F(NodeConfigurationFixture, check_unconfigured_shuttingdown_transition) {
-  auto attack_node = std::make_shared<MemoryNode>(0);
+  auto attack_node = std::make_shared<DiskNode>();
   add_node_to_executor(attack_node->get_node_base_interface());
   start_executor();
   attack_node->trigger_transition(
@@ -146,7 +89,7 @@ TEST_F(NodeConfigurationFixture, check_unconfigured_shuttingdown_transition) {
 }
 
 TEST_F(NodeConfigurationFixture, check_active_shuttingdown_transition) {
-  auto attack_node = std::make_shared<MemoryNode>(0);
+  auto attack_node = std::make_shared<DiskNode>();
   add_node_to_executor(attack_node->get_node_base_interface());
   start_executor();
   attack_node->trigger_transition(
@@ -160,7 +103,7 @@ TEST_F(NodeConfigurationFixture, check_active_shuttingdown_transition) {
 }
 
 TEST_F(NodeConfigurationFixture, check_full_node_lifecycle) {
-  auto attack_node = std::make_shared<MemoryNode>(0);
+  auto attack_node = std::make_shared<DiskNode>();
   add_node_to_executor(attack_node->get_node_base_interface());
   start_executor();
 
